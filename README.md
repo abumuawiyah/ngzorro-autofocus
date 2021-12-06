52088528fb52bc84b49f0fa65b9715833bd53967
abumuawiyah <ibnuyazit@gmail.com>
2021-12-06 16:32:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-jsayla
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jsayla)
---
+++ src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { Directive, ElementRef, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import en from '@angular/common/locales/en';
+
+import { NZ_ICONS } from 'ng-zorro-antd/icon';
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+
+import { DemoNgZorroAntdModule } from './ng-zorro-antd.module';
+
+import { NzDemoInputTextareaComponent } from './app.component';
+
+registerLocaleData(en);
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(
+  (key) => antDesignIcons[key]
+);
+
+@Directive({
+  selector: '[autofocus]',
+})
+export class AutofocusDirective {
+  constructor(private host: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.host.nativeElement.focus();
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+    ReactiveFormsModule,
+    DemoNgZorroAntdModule,
+    BrowserAnimationsModule,
+    ScrollingModule,
+    DragDropModule,
+  ],
+  declarations: [NzDemoInputTextareaComponent, AutofocusDirective],
+  bootstrap: [NzDemoInputTextareaComponent],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    { provide: NZ_ICONS, useValue: icons },
+  ],
+})
+export class AppModule {}
